765b221e70c40c8ee24cba4d3a98c61b8b29f009
aikho20 <marco.deguzman.ico@gmail.com>
2024-05-31 09:18:00 +0800

    pictures

--- components/UI/Card/index.jsx
+++ components/UI/Card/index.jsx
@@ -13,7 +13,7 @@ function Card({ title, description, onClick, resources }) {
       </p>
       <button
         onClick={() => {
-          onClick();
+          onClick()
         }}
         className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
